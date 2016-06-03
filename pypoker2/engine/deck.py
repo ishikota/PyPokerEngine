d210790981f7b40667b23c64e1aa394d4f263d09
Kota Ishimoto <ishikota086@gmail.com>
2016-06-03 23:11:09 +0900

    Create Deck class

---
+++ pypoker2/engine/deck.py
@@ -0,0 +1,45 @@
+from pypoker2.engine.card import Card
+import random
+
+class Deck:
+
+  def __init__(self, deck_ids=None, cheat=False, cheat_card_ids=[]):
+    self.cheat = cheat
+    self.cheat_card_ids = cheat_card_ids
+    self.deck = [Card.from_id(cid) for cid in deck_ids] if deck_ids else self.__setup()
+
+  def draw_card(self):
+    return self.deck.pop()
+
+  def draw_cards(self, num):
+    return reduce(lambda acc, _: acc + [self.draw_card()], range(num), [])
+
+  def size(self):
+    return len(self.deck)
+
+  def restore(self):
+    self.deck = self.__setup()
+
+  def shuffle(self):
+    if not self.cheat:
+      random.shuffle(self.deck)
+
+  # serialize format : [cheat_flg, chat_card_ids, deck_card_ids]
+  def serialize(self):
+    return [self.cheat, self.cheat_card_ids, [card.to_id() for card in self.deck]]
+
+  @classmethod
+  def deserialize(self, serial):
+    cheat, cheat_card_ids, deck_ids = serial
+    return self(deck_ids=deck_ids, cheat=cheat, cheat_card_ids=cheat_card_ids)
+
+  def __setup(self):
+    return self.__setup_cheat_deck() if self.cheat else self.__setup_52_cards()
+
+  def __setup_52_cards(self):
+    return [Card.from_id(cid) for cid in range(1,53)]
+
+  def __setup_cheat_deck(self):
+    cards = [Card.from_id(cid) for cid in self.cheat_card_ids]
+    return cards[::-1]
+
---
+++ tests/engine/deck_test.py
@@ -0,0 +1,54 @@
+from tests.base_unittest import BaseUnitTest
+from pypoker2.engine.card import Card
+from pypoker2.engine.deck import Deck
+
+class DeckTest(BaseUnitTest):
+
+  def setUp(self):
+    self.deck = Deck()
+
+  def test_draw_card(self):
+    card = self.deck.draw_card()
+    self.eq("SK", str(card))
+    self.eq(51, self.deck.size())
+
+  def test_draw_cards(self):
+    cards = self.deck.draw_cards(3)
+    self.eq("SJ", str(cards[2]))
+    self.eq(49, self.deck.size())
+
+  def test_restore(self):
+    self.deck.draw_cards(5)
+    self.deck.restore()
+    self.eq(52, self.deck.size())
+
+  def test_serialization(self):
+    self.deck.draw_cards(5)
+    self.deck.shuffle()
+    serial = self.deck.serialize()
+    restored = Deck.deserialize(serial)
+    self.eq(self.deck.cheat, restored.cheat)
+    self.eq(self.deck.deck, restored.deck)
+
+  def test_cheat_draw(self):
+    cards = [Card.from_id(cid) for cid in [12, 15, 17]]
+    cheat = Deck(cheat=True, cheat_card_ids=[12, 15, 17])
+    self.eq(cheat.draw_cards(3), cards)
+
+  def test_cheat_restore(self):
+    cards = [Card.from_id(cid) for cid in [12, 15, 17]]
+    cheat = Deck(cheat=True, cheat_card_ids=[12, 15, 17])
+    cheat.draw_cards(2)
+    cheat.restore()
+    self.eq(cheat.draw_cards(3), cards)
+
+  def test_cheat_serialization(self):
+    cards = [Card.from_id(cid) for cid in [12, 15, 17]]
+    cheat = Deck(cheat=True, cheat_card_ids=[12, 15, 17])
+    serial = cheat.serialize()
+    restored = Deck.deserialize(serial)
+    self.eq(cheat.deck, restored.deck)
+    self.eq(cheat.cheat, restored.cheat)
+    self.eq(cheat.cheat_card_ids, restored.cheat_card_ids)
+
+
