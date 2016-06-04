4d9fa842f11451a4ba54b84d059bb67dbf39f7a2
Kota Ishimoto <ishikota086@gmail.com>
2016-06-04 10:35:10 +0900

    Create Seats class

---
+++ pypoker2/engine/seats.py
@@ -0,0 +1,20 @@
+from pypoker2.engine.pay_info import PayInfo
+
+class Seats:
+
+  def __init__(self):
+    self.players = []
+
+  def sitdown(self, player):
+    self.players.append(player)
+
+  def size(self):
+    return len(self.players)
+
+  def count_active_players(self):
+    return len([p for p in self.players if p.is_active()])
+
+  def count_ask_wait_players(self):
+    is_paying = lambda player: player.pay_info.status == PayInfo.PAY_TILL_END
+    return len([p for p in self.players if is_paying(p)])
+
---
+++ tests/engine/seats_test.py
@@ -0,0 +1,40 @@
+from tests.base_unittest import BaseUnitTest
+from pypoker2.engine.player import Player
+from pypoker2.engine.seats import Seats
+
+class SeatsTest(BaseUnitTest):
+
+  def setUp(self):
+    self.seats = Seats()
+    self.p1 = Player("uuid1", 100)
+    self.p2 = Player("uuid2", 100)
+    self.p3 = Player("uuid3", 100)
+
+  def test_sitdown(self):
+    self.seats.sitdown(self.p1)
+    self.true(self.p1 in self.seats.players)
+
+  def test_size(self):
+    self.__sitdown_players()
+    self.eq(3, len(self.seats.players))
+
+  def test_count_active_players(self):
+    self.__setup_pay_status()
+    self.__sitdown_players()
+    self.eq(2, self.seats.count_active_players())
+
+  def test_acount_ask_wait_players(self):
+    self.__setup_pay_status()
+    self.__sitdown_players()
+    self.eq(1, self.seats.count_ask_wait_players())
+
+
+  def __setup_pay_status(self):
+    self.p1.pay_info.update_by_pay(10)
+    self.p2.pay_info.update_to_fold()
+    self.p3.pay_info.update_to_allin()
+
+  def __sitdown_players(self):
+    for player in [self.p1, self.p2, self.p3]:
+      self.seats.sitdown(player)
+
