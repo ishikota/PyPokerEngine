777195d4fe7718746ca4b64e0e52e347e20146a5
Kota Ishimoto <ishikota086@gmail.com>
2016-06-05 13:39:39 +0900

    Add payinfo serialization method

--- pypoker2/engine/pay_info.py
+++ pypoker2/engine/pay_info.py
@@ -4,9 +4,9 @@ class PayInfo:
   ALLIN  = 1
   FOLDED = 2
 
-  def __init__(self):
-    self.amount = 0
-    self.status = self.PAY_TILL_END
+  def __init__(self, amount=0, status=0):
+    self.amount = amount
+    self.status = status
 
   def update_by_pay(self, amount):
     self.amount += amount
@@ -17,3 +17,10 @@ class PayInfo:
   def update_to_allin(self):
     self.status = self.ALLIN
 
+  # serialize format : [amount, status]
+  def serialize(self):
+    return [self.amount, self.status]
+
+  @classmethod
+  def deserialize(self, serial):
+    return self(amount=serial[0], status=serial[1])
--- tests/engine/pay_info_test.py
+++ tests/engine/pay_info_test.py
@@ -21,3 +21,10 @@ class PayInfoTest(BaseUnitTest):
     self.eq(0, self.info.amount)
     self.eq(PayInfo.FOLDED, self.info.status)
 
+  def test_serialization(self):
+    self.info.update_by_pay(100)
+    self.info.update_to_allin()
+    serial = self.info.serialize()
+    restored = PayInfo.deserialize(serial)
+    self.eq(100, restored.amount)
+    self.eq(PayInfo.ALLIN, restored.status)
