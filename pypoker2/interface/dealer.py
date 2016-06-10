c26974fd7056da50633d4b850d13f1ad2568da20
Kota Ishimoto <ishikota086@gmail.com>
2016-06-10 13:45:12 +0900

    Complete Dealer

--- pypoker2/interface/dealer.py
+++ pypoker2/interface/dealer.py
@@ -1,23 +1,30 @@
+from pypoker2.engine.poker_constants import PokerConstants as Const
 from pypoker2.engine.table import Table
+from pypoker2.engine.player import Player
 from pypoker2.engine.round_manager import RoundManager
+from pypoker2.engine.message_builder import MessageBuilder
+from pypoker2.interface.message_handler import MessageHandler
+import random
 
 class Dealer:
 
   def __init__(self, small_blind_amount=None, initial_stack=None):
     self.small_blind_amount = small_blind_amount
     self.initial_stack = initial_stack
+    self.uuid_list = self.__generate_uuid_list()
+    self.message_handler = MessageHandler()
+    self.table = Table()
 
   def register_player(self, player_name, algorithm):
     self.__config_check()
     uuid = self.__escort_player_to_table(player_name)
     self.__register_algorithm_to_message_handler(uuid, algorithm)
 
-  def start_game(self, round_num):
-    for round_count in range(1, round_num+1):
-      self.__play_round()
-
-  def summary_game_result(self):
-    pass
+  def start_game(self, max_round):
+    table = self.table
+    for round_count in range(1, max_round+1):
+      table = self.__play_round(round_count, self.small_blind_amount, table)
+    return self.__generate_game_result(max_round, table.seats)
 
   def set_small_blind_amount(self, amount):
     self.small_blind_amount = amount
@@ -26,14 +33,44 @@ class Dealer:
     self.initial_stack = amount
 
 
-  def __register_algorithm_to_message_handler(self, algorithm):
-    pass
+  def __register_algorithm_to_message_handler(self, uuid, algorithm):
+    self.message_handler.register_algorithm(uuid, algorithm)
 
   def __escort_player_to_table(self, player_name):
-    pass
+    uuid = self.__fetch_uuid()
+    player = Player(uuid, self.initial_stack, player_name)
+    self.table.seats.sitdown(player)
+    return uuid
 
-  def __play_round(self):
-    pass
+  def __play_round(self, round_count, blind_amount, table):
+    state, msgs = RoundManager.start_new_round(round_count, blind_amount, table)
+    while state["street"] != Const.Street.FINISHED:
+      self.__message_check(msgs)
+      action, bet_amount = self.__publish_messages(msgs)
+      state, msgs = RoundManager.apply_action(state, action, bet_amount)
+    return state["table"]
+
+  def __message_check(self, msgs):
+    address, msg = msgs[-1]
+    if address == -1 or msg["type"] != 'ask':
+      raise Exception("Last message is not ask type. : %s" % msgs)
+
+  def __publish_messages(self, msgs):
+    for address, msg in msgs[:-1]:
+      self.message_handler.process_message(address, msg)
+    return self.message_handler.process_message(*msgs[-1])
+
+  def __generate_game_result(self, max_round, seats):
+    config = self.__gen_config(max_round)
+    return MessageBuilder.build_game_result_message(config, seats)["message"]
+
+  def __gen_config(self, max_round):
+    return {
+        "initial_stack": self.initial_stack,
+        "max_round": max_round,
+        "small_blind_amount": self.small_blind_amount
+    }
+
 
   def __config_check(self):
     if self.small_blind_amount is None:
@@ -42,4 +79,15 @@ class Dealer:
     if self.initial_stack is None:
       raise Exception("initial_stack is not set!!\
           You need to call 'dealer.set_initial_stack' before.")
+
+  def __fetch_uuid(self):
+    return self.uuid_list.pop()
+
+  def __generate_uuid_list(self):
+    return [self.__generate_uuid() for _ in range(100)]
+
+  def __generate_uuid(self):
+    uuid_size = 22
+    chars = [chr(code) for code in range(97,123)]
+    return "".join([random.choice(chars) for _ in range(uuid_size)])
 
---
+++ tests/interface/dealer_test.py
@@ -0,0 +1,29 @@
+from tests.base_unittest import BaseUnitTest
+from mock import patch
+from pypoker2.interface.dealer import Dealer
+from pypoker2.players.sample.fold_man import PokerPlayer as FoldMan
+
+class DealerTest(BaseUnitTest):
+
+  def setUp(self):
+    self.dealer = Dealer(5, 100)
+    self.mh = self.dealer.message_handler
+
+  def test_register_poker_player(self):
+    algo = FoldMan()
+    with patch.object(self.dealer, '_Dealer__fetch_uuid', return_value="a"):
+      self.dealer.register_player("hoge", algo)
+      player = self.dealer.table.seats.players[0]
+      self.eq("hoge", player.name)
+      self.eq(100, player.stack)
+      self.eq(algo, self.mh.algo_owner_map["a"])
+
+  def test_play_a_round(self):
+    algos = [FoldMan() for _ in range(2)]
+    [self.dealer.register_player(name, algo) for name, algo in zip(["hoge", "fuga"], algos)]
+    players = self.dealer.table.seats.players
+    summary = self.dealer.start_game(1)
+    player_state = summary["game_information"]["seats"]
+    self.eq(95, player_state[0]["stack"])
+    self.eq(105, player_state[1]["stack"])
+
