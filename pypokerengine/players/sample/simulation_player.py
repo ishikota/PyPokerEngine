c97216354f15660f82e2e2fc245ad3a1f124b760
Kota Ishimoto <ishikota086@gmail.com>
2016-09-04 15:42:32 +0900

    Create simulation based player

--- pypokerengine/players/sample/simulation_player.py
+++ pypokerengine/players/sample/simulation_player.py
@@ -13,31 +13,53 @@ class PokerPlayer(BasePokerPlayer):
   def __init__(self):
     self.simulator = pypokerengine.players.round_simulator.RoundSimualtor()
     self.small_blind_amount = 5
+    self.simulation_times = 100
 
   def set_small_blind_amount(self, amount):
     self.small_blind_amount = amount
 
   def declare_action(self, hole_card, valid_actions, round_info, action_histories):
     round_state = self.__restore_round_state(hole_card, round_info, action_histories)
-    result_holder = {}
-    # Simulate all actions for simulation_times
+    simulation_results = self.__simulate_all_actions(self.simulation_times, hole_card, valid_actions, round_state)
+    return self.__decide_action(valid_actions, simulation_results)
+
+  def __decide_action(self, valid_actions, simulation_results):
+    rewards = [self.__research_reward(action, simulation_results[action]) \
+        for action in simulation_results.iterkeys()]
+    best_action_idx = rewards.index(max(rewards))
+    best_action = valid_actions[best_action_idx]
+    action, amount = best_action["action"], best_action["amount"]
+    if action == "raise":
+      amount = best_action["amount"]["min"]
+    print "Best action = %s:%d (rewards = %s)" % (action, amount, rewards)
+    return action, amount
+
+  def __research_reward(self, action, simulation_results):
+    seats_results = [result["seats"]  for result in simulation_results]
+    my_results = [[seat for seat in seats if seat["uuid"] == self.uuid][0] for seats in seats_results]
+    stack_info = [result["stack"] for result in my_results]
+    print "Simulation result of %s => min:%d, max:%d, average:%d" %\
+      (action, min(stack_info), max(stack_info), sum(stack_info)*1.0/len(stack_info))
+    return sum(stack_info) * 1.0 / len(stack_info)
+
+  def __simulate_all_actions(self, simulation_times, hole_info, valid_actions, original_state):
+    simulation_results = {}
     for valid_action in valid_actions:
-      action = valid_action["action"]
-      amount = valid_action["amount"]["min"] if action == "raise" else valid_action["amount"]
-      apply_action_info = self.simulator.gen_action_info(action, amount)
-      result_holder[action] = []
-      for i in range(100):
-        copy_state = self.__deep_copy_state(round_state)
-        table = copy_state["table"]
-        self.__attach_holecard_at_random(table.seats.players, hole_card, table.deck)
-        result = self.simulator.start_simulation(copy_state, apply_action_info)
-        result_holder[action].append(result)
-    for action in result_holder.iterkeys():
-      seats_results = [result["seats"]  for result in result_holder[action]]
-      my_results = [[seat for seat in seats if seat["uuid"] == self.uuid][0] for seats in seats_results]
-      rewards = [result["stack"] for result in my_results]
-      print "Simulation result of %s => min:%d, max:%d, average:%d" % (action, min(rewards), max(rewards), sum(rewards)*1.0/len(rewards))
-    return 'fold', 0
+      apply_action_info = self.__gen_apply_action(valid_action)
+      simulation_results[valid_action["action"]] = \
+          [self.__simulate_action(hole_info, original_state, apply_action_info) for _ in range(simulation_times)]
+    return simulation_results
+
+  def __simulate_action(self, hole_info, original_state, apply_action_info):
+    copy_state = self.__deep_copy_state(original_state)
+    table = copy_state["table"]
+    self.__attach_holecard_at_random(table.seats.players, hole_info, table.deck)
+    return self.simulator.start_simulation(copy_state, apply_action_info)
+
+  def __gen_apply_action(self, action_info):
+    action = action_info["action"]
+    amount = action_info["amount"]["min"] if action == "raise" else action_info["amount"]
+    return self.simulator.gen_action_info(action, amount)
 
   def receive_game_start_message(self, game_info):
     self.__setup_algorithm_for_each_player(game_info["seats"])
