2c39da0301d770b040beed79d27e118d26849dc9
Kota Ishimoto <ishikota086@gmail.com>
2016-12-10 14:59:19 +0900

    Improve visualization

--- pypokerengine/utils/visualize_utils.py
+++ pypokerengine/utils/visualize_utils.py
@@ -10,12 +10,13 @@ def visualize_game_start(game_info, uuid=None):
     ls.append(_visualize_item("start stack = %s" % game_info["rule"]["initial_stack"]))
     ls.append(_visualize_item("       ante = %s" % game_info["rule"]["ante"]))
     ls.append(_visualize_item("small blind = %s" % game_info["rule"]["small_blind_amount"]))
-    ls.append(_visualize_item("blind structure"))
-    for round_count in game_info["rule"]["blind_structure"]:
-        level_info = game_info["rule"]["blind_structure"][round_count]
-        ante, sb_amount = level_info["ante"], level_info["small_blind"]
-        ls.append(_visualize_sub_item("after %d round : ante = %s, sb_amount = %s" %
-            (round_count, ante, sb_amount)))
+    if len(game_info["rule"]["blind_structure"]) != 0:
+        ls.append(_visualize_item("blind structure"))
+        for round_count in game_info["rule"]["blind_structure"]:
+            level_info = game_info["rule"]["blind_structure"][round_count]
+            ante, sb_amount = level_info["ante"], level_info["small_blind"]
+            ls.append(_visualize_sub_item("after %d round : ante = %s, sb_amount = %s" %(
+                round_count, ante, sb_amount)))
     ls.append(DIVIDER)
     return "\n".join(ls)
 
@@ -121,7 +122,17 @@ def visualize_round_state(rs):
     for idx, player_info in enumerate(rs["seats"]):
         player_str = visualize_player_with_badge(player_info, rs)
         player_str = player_str.replace("NEXT", "CURRENT")
-        ls.append(_visualize_item("%d : %s" % (idx, player_str)))
+        ls.append(_visualize_sub_item("%d : %s" % (idx, player_str)))
+    ls.append(_visualize_item("action histories"))
+    sort_key = lambda e: {"preflop":0, "flop":1, "turn":2, "river":3}[e[0]]
+    for street, histories in sorted(rs["action_histories"].items(), key=sort_key):
+        if len(histories) != 0:
+            ls.append(_visualize_sub_item(street))
+            for history in histories:
+                summary = {k:v for k,v in history.items()}
+                uuid = summary.pop("uuid")
+                summary["player"] = "%s (uuid=%s)" % (_fetch_player_name(uuid, rs), uuid)
+                ls.append(_visualize_sub_sub_item(str(summary)))
     return "\n".join(ls)
 
 def visualize_player_with_badge(player, rs):
@@ -160,4 +171,7 @@ def _visualize_item(item):
 
 def _visualize_sub_item(subitem):
     return "    - %s" % subitem
+
+def _visualize_sub_sub_item(subsubitem):
+    return "      - %s" % subsubitem
 
