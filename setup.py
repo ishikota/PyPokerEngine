187d3023311f09a0aeec11426a478b88fd866fd4
Kota Ishimoto <ishikota086@gmail.com>
2016-12-10 15:08:25 +0900

    Bump up version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'PyPokerEngine',
-    version = '0.1.3',
+    version = '0.1.4',
     author = 'ishikota',
     author_email = 'ishikota086@gmail.com',
     description = 'Poker engine for poker AI development in Python ',
